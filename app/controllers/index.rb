104deb3ff224583fadcda2dfb236eb496af1f1b0
Travis75 <travis.gibson75@gmail.com>
2014-10-16 17:04:00 -0700

    layout slightly altered...fixed css issues added background and js functionality for fade in

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,6 +4,24 @@ get '/' do
   erb :index
 end
 
+#This is Clarifai solution. Use if there are no bugs with Clarifai.
+get '/image_tag' do
+  # ["pavement", "nobody", "empty", "line", "stairs", "stride", "road", "house", "industry", "street", "shadow", "energy", "sidewalk", "perspective", "picker", "building", "row", "daytime", "urban", "roof"]
+
+  response = HTTParty.post('https://api.clarifai.com/v1/token/', query: {grant_type: "client_credentials", client_id: "K5NwdL1Kke7KlgO0PDGQL79wT93q12lpsVJOC6a5", client_secret: "JsQ4_FXp-d_7W01KrGhvKMKPMEJ0bnTLDbETkIop"})
+
+  token = response["access_token"]
+
+  words = HTTParty.get("https://api.clarifai.com/v1/tag/?url=#{params[:image_url]}", headers: {"Authorization" => "Bearer #{token}"})
+
+  p words["results"][0]["result"]["tag"]["classes"]
+
+  finished_haiku = Haiku.new(words["results"][0]["result"]["tag"]["classes"])
+  finished_haiku.create_haiku
+
+  content_type :json
+    finished_haiku.haiku.to_json
+end
 
 # While Clarifai is in beta/if there is a problem with beta or rate limiting occurs this is the workaround solution to continue to grab enough words for a haiku using another API (Alchemy API).
 
@@ -22,21 +40,3 @@ end
 #   content_type :json
 #     finished_haiku.haiku.to_json
 # end
-
-
-#This is Clarifai solution. Use if there are no bugs with Clarifai.
-get '/image_tag' do
-  # response = HTTParty.post('https://api.clarifai.com/v1/token/', query: {grant_type: "client_credentials", client_id: "K5NwdL1Kke7KlgO0PDGQL79wT93q12lpsVJOC6a5", client_secret: "JsQ4_FXp-d_7W01KrGhvKMKPMEJ0bnTLDbETkIop"})
-
-  # token = response["access_token"]
-
-  # words = HTTParty.get("https://api.clarifai.com/v1/tag/?url=#{params[:image_url]}", headers: {"Authorization" => "Bearer #{token}"})
-
-  # p words["results"][0]["result"]["tag"]["classes"]
-
-  finished_haiku = Haiku.new(["pavement", "nobody", "empty", "line", "stairs", "stride", "road", "house", "industry", "street", "shadow", "energy", "sidewalk", "perspective", "picker", "building", "row", "daytime", "urban", "roof"])
-  finished_haiku.create_haiku
-
-  content_type :json
-    finished_haiku.haiku.to_json
-end
--- public/js/pictures.js
+++ public/js/pictures.js
@@ -9,12 +9,14 @@ $(document).ready(function() {
       data: {image_url: picture_to_tag}
     }).done(function(data){
       console.log(data)
-        $('#user_photo').css('background-image', 'url' + '(' + picture_to_tag + ')');
+        $('#user-photo').append("<img src =" + picture_to_tag + ">")
       $.each(data, function(index, value){
         console.log(value)
         $("#text").append('<span></span><br>')
         pMessage('#text span', value.join(' ').split(''))
       });
+        $("#user-photo").animate({opacity: 1}, 3000)
+        $("#text").animate({opacity: 1}, 3000)
     });
   });
 })
