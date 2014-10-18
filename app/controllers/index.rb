8a3c62115816f09e7a7077cfef7f9ad00de509b7
Travis75 <travis.gibson75@gmail.com>
2014-10-17 18:48:46 -0700

    percentages

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -8,8 +8,7 @@ end
 get '/image_tag' do
   #stub_data = ["pavement", "nobody", "empty", "line", "stairs", "stride", "road", "house", "industry", "street", "shadow", "energy", "sidewalk", "perspective", "picker", "building", "row", "daytime", "urban", "roof"]
 
-  response = HTTParty.post('https://api.clarifai.com/v1/token/', query: {grant_type: "client_credentials", client_id: "fNrszjnk-grLt_v9v0bw3NPst6DcI43ZgGyn7TFE", client_secret: "Hq0XfUAXbKgUEtLS3p4tPU9ZKvsxAFRkDvwU2DD2"})
-  # response = HTTParty.post('https://api.clarifai.com/v1/token/', query: {grant_type: "client_credentials", client_id: ENV["CLIENT_ID"], client_secret: ENV["CLIENT_SECRET"]})
+  response = HTTParty.post('https://api.clarifai.com/v1/token/', query: {grant_type: "client_credentials", client_id: ENV["CLIENT_ID"], client_secret: ENV["CLIENT_SECRET"]})
   token = response["access_token"]
   words = HTTParty.get("https://api.clarifai.com/v1/tag/?url=#{params[:image_url]}", headers: {"Authorization" => "Bearer #{token}"})
   finished_haiku = Haiku.new(words["results"][0]["result"]["tag"]["classes"])
@@ -17,6 +16,7 @@ get '/image_tag' do
   content_type :json
     finished_haiku.haiku.to_json
 end
+
 
 # While Clarifai is in beta/if there is a problem with beta or rate limiting occurs this is the workaround solution to continue to grab enough words for a haiku using another API (Alchemy API).
 
